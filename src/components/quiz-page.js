93422d1a7639d31b8f84f55415d09c2817b0b674
Sebastian-XIV <seva.kavalenka@gmail.com>
2025-03-05 19:43:52 +0100

    Min-height for body added

--- src/components/quiz-page.js
+++ src/components/quiz-page.js
@@ -48,7 +48,7 @@ export default function Quiz() {
     setOptions([]);
     setNewData([]);
     setReadyForCheck(false);
-    // window.scrollTo(0, 0)
+    window.scrollTo(0, 0)
   }
 
   const questionElements = newData.map((item, questionIndex) => (
